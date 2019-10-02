6a456f63a5de338db3bfa07b75e885e385a4744e
mpattok <55061963+mpattok@users.noreply.github.com>
2019-10-01 23:41:14 -0400

    Create builder.js

---
+++ resources/builder.js
@@ -0,0 +1,10 @@
+heading.innerHTML = `
+<header style="background-color: #858585;">
+<div width="100%" style="margin: 0 auto; position: relative;">
+<div style="position: fixed;">image here</div>
+<div style="float: right;">text here</div>
+</div>
+<nav width="100%" style="border-top: 1px solid black; border-right: 1px solid black;">`
+
+</nav>
+</header>`;
