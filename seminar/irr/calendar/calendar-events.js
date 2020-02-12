46387d5a45aec8ff2f1bc200c98353fc244f7734
mpattok <55061963+mpattok@users.noreply.github.com>
2020-02-12 14:21:06 -0500

    Create calendar-events.js

---
+++ seminar/irr/calendar/calendar-events.js
@@ -0,0 +1,15 @@
+/*
+  format: [["DATE CODE","EVENT INFO"],...]
+  TRUE/FALSE refers to whether or not an event goes on list schedule (joke items like "Wish Matt a happy birthday :)" would have false for that value)
+*/
+
+var events = [
+["2-14-2020","<b>TMP SCRIPT DONE</b><br>END OF DAY"],
+["2-17-2020","<b>
+[],
+["2-19-2020",<b>TMP SLIDES DONE</b><br>END OF DAY"],
+[],
+[],
+["2-25-2020","<b>PRACTICE TMP RECORDING</b><br>Matts's room after school for 20ish minutes"],
+["2-28-2020","<b>PRESENT TMP"],
+];
