581bf4b6da329161a81f505b12b93e89599d2b5a
mpattok <55061963+mpattok@users.noreply.github.com>
2021-04-21 10:21:06 -0400

    Add files via upload

---
+++ schedule/calendar-events.js
@@ -0,0 +1,15 @@
+/*
+  format: [["DATE CODE","EVENT INFO",TRUE/FALSE],...]
+  TRUE/FALSE refers to whether or not an event goes on list schedule (joke items like "Wish Matt a happy birthday :)" would have false for that value)
+*/
+
+var events = [
+  ["5-6-2021", "Computer Science A (12 PM)", true],
+  ["5-10-2021", "Macroeconomics (12 PM)", true],
+  ["5-12-2021", "<p style='text-indent: -12px; margin: 0; padding: 0; margin-left: 12px'>Language (8 AM)</p><p>Microeconomics (12 PM)</p>", true],
+  ["5-14-2021", "Biology (8 AM)", true],
+  ["5-17-2021", "<p style='text-indent: -12px; margin: 0; padding: 0; margin-left: 12px'>Computer Science Principles (8 AM)</p><p>Statistics (12 PM)</p>", true],
+  ["5-20-2021", "<p style='text-indent: -12px; margin: 0; padding: 0; margin-left: 12px'><b>Digital Portfolios Due 11:59 PM EST:</b></p><p style='text-indent: -12px; margin: 0; padding: 0; margin-left: 12px'>Computer Science Principles</p><p style='margin: 0; padding: 0'>Research</p>", true]
+];
+
+
