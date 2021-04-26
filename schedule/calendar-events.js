f40f7def459d16e2342133cfb16f0331274575f1
mpattok <55061963+mpattok@users.noreply.github.com>
2021-04-26 13:08:25 -0400

    Update calendar-events.js

--- schedule/calendar-events.js
+++ schedule/calendar-events.js
@@ -9,7 +9,7 @@ var events = [
   ["5-12-2021", "<p style='text-indent: -12px; margin: 0; padding: 0; margin-left: 12px'>Language (8 AM)</p><p>Microeconomics (12 PM)</p>", true],
   ["5-14-2021", "Biology (8 AM)", true],
   ["5-17-2021", "<p style='text-indent: -12px; margin: 0; padding: 0; margin-left: 12px'>Computer Science Principles (8 AM)</p><p>Statistics (12 PM)</p>", true],
-  ["5-20-2021", "<p style='text-indent: -12px; margin: 0; padding: 0; margin-left: 12px'><b>Digital Portfolios Due 11:59 PM EST:</b></p><p style='text-indent: -12px; margin: 0; padding: 0; margin-left: 12px'>Computer Science Principles</p><p style='margin: 0; padding: 0'>Research</p>", true]
+  ["5-20-2021", "<p style='text-indent: -12px; margin: 0; padding: 0; margin-left: 12px'><b>Digital Portfolios Due 11:59 PM EST:</b></p><p style='text-indent: -12px; margin: 0; padding: 0; margin-left: 12px'>Computer Science Principles</p><p style='margin: 0; padding: 0'>Research</p>", true],
   ["7-5-3021", "AP Scores Released in Florida, Louisiana, Mississippi, Texas", false],
   ["7-8-3021", "AP Scores Released in Michigan", true]
 ];
