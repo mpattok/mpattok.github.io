6e649b4acf941e278e35bf1e4a5115d452416cc0
mpattok <55061963+mpattok@users.noreply.github.com>
2021-06-04 13:15:15 -0400

    Update calendar-events.js

--- schedule/calendar-events.js
+++ schedule/calendar-events.js
@@ -3,6 +3,9 @@
   TRUE/FALSE refers to whether or not an event goes on list schedule (joke items like "Wish Matt a happy birthday :)" would have false for that value)
 */
 
+var parking = "6:30-7:30 PM: Collecting parking money for Math Team at ACE parking lot for plaza music event";
+var bovard = "Bovard Scholars";
+
 var events = [
   ["5-6-2021", "Computer Science A (12 PM)", true],
   ["5-10-2021", "Macroeconomics (12 PM)", true],
@@ -10,8 +13,20 @@ var events = [
   ["5-14-2021", "Biology (8 AM)", true],
   ["5-17-2021", "<p style='text-indent: -12px; margin: 0; padding: 0; margin-left: 12px'>Computer Science Principles (8 AM)</p><p>Statistics (12 PM)</p>", true],
   ["5-20-2021", "<p style='text-indent: -12px; margin: 0; padding: 0; margin-left: 12px'><b>Digital Portfolios Due 11:59 PM EST:</b></p><p style='text-indent: -12px; margin: 0; padding: 0; margin-left: 12px'>Computer Science Principles</p><p style='margin: 0; padding: 0'>Research</p>", true],
-  ["7-5-3021", "AP Scores Released in Florida, Louisiana, Mississippi, Texas", false],
-  ["7-8-3021", "AP Scores Released in Michigan", true]
+  ["6-2-2021", parking, true],
+  ["6-9-2021", parking, true].
+  ["6-16-2021", parking, true].
+  ["6-23-2021", parking, true],
+  ["6-25-2021", parking, true].
+  ["6-30-2021", parking, true],
+  ["7-7-2021", parking, true],
+  ["7-21-2021", "AP Scores Released", true],
+  ["8-4-2021", parking, true],
+  ["8-11-2021", parking, true],
+  ["8-13-2021", parking, true],
+  ["8-18-2021", parking, true],
+  ["8-20-2021", parking, true],
+  ["8-25-2021", parking, true]
 ];
 
 
